7527e2b8684ec1eae18a32f294f00a242538f328
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-07-08 17:10:45 -0500

    Add files via upload

--- scripts/loadTopbar.js
+++ scripts/loadTopbar.js
@@ -1,25 +1,29 @@
+// loadTopbar.js
 document.addEventListener('DOMContentLoaded', function() {
-    const topbar = document.getElementById('topbar');
-    topbar.innerHTML = `
-        <div class="topbar-container">
-            <button id="menuButton" class="menu-button"></button>
-            <img src="../images/nnlogo.png" alt="NeuralNet Logo" class="logo">
-            <img src="../images/magnifying-glass.png" alt="Search" class="search-icon">
-            <img src="../images/profile-icon.png" alt="Profile" class="profile-icon">
-        </div>
-        <nav id="menu" class="menu hidden">
-            <ul>
-                <li><a href="https://aurelius-in.github.io/neuralnet.press/">Home</a></li>
-                <li><a href="https://aurelius-in.github.io/neuralnet.press/issues.html">Issues</a></li>
-                <li><a href="https://aurelius-in.github.io/neuralnet.press/#about">About Us</a></li>
-                <li><a href="https://aurelius-in.github.io/neuralnet.press/#contact">Contact</a></li>
-            </ul>
-        </nav>
-    `;
+    const pathToTopbar = location.pathname.includes('/articles/') ? '../topbar.html' : 'topbar.html';
+    const headerElement = location.pathname.includes('/articles/') ? document.querySelector('.issue-detail-header') : document.querySelector('header');
+    console.log('Loading topbar from:', pathToTopbar);
 
-    const menuButton = document.getElementById('menuButton');
-    const menu = document.getElementById('menu');
-    menuButton.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-    });
+    fetch(pathToTopbar)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('topbar-placeholder').innerHTML = data;
+
+            // Event listeners for topbar
+            const menuToggle = document.getElementById('menu-toggle');
+            const navLinks = document.getElementById('nav-links');
+            menuToggle.addEventListener('click', () => {
+                navLinks.classList.toggle('active');
+            });
+
+            if (headerElement) {
+                headerElement.appendChild(document.getElementById('topbar-placeholder').children[0]);
+            }
+        })
+        .catch(error => console.error('Error loading topbar:', error));
 });
