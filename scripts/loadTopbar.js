babf6cb6de2f67a3142c0bd8b171f987f4cc7352
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-07-08 17:01:53 -0500

    Update loadTopbar.js

--- scripts/loadTopbar.js
+++ scripts/loadTopbar.js
@@ -1,29 +1,31 @@
-// loadTopbar.js
+function loadTopbar() {
+    var topbarHtml = `
+        <header>
+            <button class="menu-button" onclick="toggleMenu()">&#9776;</button>
+            <img src="../images/nnlogo.png" alt="NeuralNet">
+            <nav>
+                <a href="../index.html">Home</a>
+                <a href="../issues.html">Issues</a>
+                <a href="../#about">About Us</a>
+                <a href="../#contact">Contact</a>
+            </nav>
+            <div>
+                <a href="#"><img src="../images/magnifying-glass.png" alt="Search"></a>
+                <a href="#"><img src="../images/profile.png" alt="Profile"></a>
+            </div>
+        </header>
+    `;
+
+    document.getElementById("topbar").innerHTML = topbarHtml;
+}
+
+function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.style.display = (nav.style.display === 'flex') ? 'none' : 'flex';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    const pathToTopbar = location.pathname.includes('/articles/') ? '../topbar.html' : 'topbar.html';
-    const headerElement = location.pathname.includes('/articles/') ? document.querySelector('.issue-detail-header') : document.querySelector('header');
-    console.log('Loading topbar from:', pathToTopbar);
-
-    fetch(pathToTopbar)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('topbar-placeholder').innerHTML = data;
-
-            // Event listeners for topbar
-            const menuToggle = document.getElementById('menu-toggle');
-            const navLinks = document.getElementById('nav-links');
-            menuToggle.addEventListener('click', () => {
-                navLinks.classList.toggle('active');
-            });
-
-            if (headerElement) {
-                headerElement.appendChild(document.getElementById('topbar-placeholder').children[0]);
-            }
-        })
-        .catch(error => console.error('Error loading topbar:', error));
+    loadTopbar();
+    var menuButton = document.querySelector('.menu-button');
+    menuButton.addEventListener('click', toggleMenu);
 });
